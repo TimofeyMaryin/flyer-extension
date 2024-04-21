c58c7db61c405acab926f4bb95ee1152fe58d3ca
Timofey Maryin <nemotim4ic@gmail.com>
2024-04-21 21:59:51 +0500

    Remove Appsflyer condition

--- app/src/main/java/custom/lib/droid/appsflyerextension/MainActivity.kt
+++ app/src/main/java/custom/lib/droid/appsflyerextension/MainActivity.kt
@@ -1,22 +1,47 @@
 package custom.lib.droid.appsflyerextension
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import custom.lib.droid.appsflyerextension.ui.theme.AppsflyerExtensionTheme
 import custom.lib.droid.flyer_extension.FlyerExtension
+import custom.lib.droid.flyer_extension.isNDaysPassed
+import java.time.LocalDate
 
 class MainActivity : ComponentActivity() {
+
+    var text by mutableStateOf("")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("TAG", "onCreate: ${isNDaysPassed(startDate = LocalDate.of(2024, 4, 21), 3)}", )
+        Log.e("TAG", "onCreate: ${isNDaysPassed(startDate = LocalDate.of(2024, 4, 15), 4)}", )
+        FlyerExtension.setExtension(
+            value = "i76DHWyqpCeUdgbXaj9XmL",
+            context = applicationContext,
+            onError = {
+                Log.e("TAG", "onCreate: onError call", )
+                text = "onError"
+            },
+            onSuccess = {
+                Log.e("TAG", "onCreate: onSuccess call. ${it.content}", )
+                text = "onSuccess"
+            },
+            startDate = LocalDate.of(2024, 4, 18)
+        )
 
         setContent {
             AppsflyerExtensionTheme {
@@ -26,7 +51,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 
-
+                    Text(text = "Test APPSFLYER NON-ORGANIC TEST: $text")
                 }
             }
         }
--- flyer-extension/src/main/java/custom/lib/droid/flyer_extension/FlyerExtension.kt
+++ flyer-extension/src/main/java/custom/lib/droid/flyer_extension/FlyerExtension.kt
@@ -1,9 +1,13 @@
 package custom.lib.droid.flyer_extension
 
 import android.content.Context
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import com.appsflyer.AppsFlyerConversionListener
 import com.appsflyer.AppsFlyerLib
 import custom.lib.droid.decrypt_helper.EncryptionHelper
+import java.time.LocalDate
 import java.util.Locale
 import java.util.Timer
 import java.util.TimerTask
@@ -21,6 +25,7 @@ object FlyerExtension {
     fun setExtension(
         value: String,
         context: Context,
+        startDate: LocalDate,
         onError: () -> Unit,
         onSuccess: (FlyerModel) -> Unit
     ) {
@@ -37,9 +42,9 @@ object FlyerExtension {
         }
 
         val conversionListener = object : AppsFlyerConversionListener {
+            @RequiresApi(Build.VERSION_CODES.O)
             override fun onConversionDataSuccess(conversionData: MutableMap<String, Any>) {
 
-                val isOrganic = conversionData["af_status"].toString().lowercase() == "organic"
                 MEDIA_SOURCE = conversionData["media_source"].toString()
                 AF_SITEID = conversionData["af_siteid"].toString()
                 ADSET = conversionData["adset"].toString()
@@ -47,29 +52,34 @@ object FlyerExtension {
                 ADGROUP = conversionData["adgroup"].toString()
                 AF_AD = conversionData["af_ad"].toString()
                 res = FlyerStatus.SUCCESS
-
-                if (!isOrganic) {
-                    if (CheckPush.checkPush(context)) {
-                        onSuccess(
-                            FlyerModel(
-                                status = FlyerStatus.SUCCESS,
-                                content = "?media_source=$MEDIA_SOURCE" + "&af_siteid=$AF_SITEID" + "&campaign=${CAMPAIGN}" + "&adgroup=$ADGROUP" + "&adset=$ADSET" + "&af_ad=$AF_AD"
-                            )
+                Log.e("TAG", "onConversionDataSuccess: ${conversionData["af_status"].toString().lowercase()}", )
+                Log.e("TAG", "onConversionDataSuccess: ${conversionData["af_status"].toString().lowercase() == "organic"}", )
+                if (CheckPush.checkPush(context) && isNDaysPassed(startDate, 2)) {
+                    Log.e("TAG", "onConversionDataSuccess: IS NON ORGANI. CONDITION SUCCESS.", )
+                    onSuccess(
+                        FlyerModel(
+                            status = FlyerStatus.SUCCESS,
+                            content = "?media_source=$MEDIA_SOURCE" + "&af_siteid=$AF_SITEID" + "&campaign=${CAMPAIGN}" + "&adgroup=$ADGROUP" + "&adset=$ADSET" + "&af_ad=$AF_AD"
                         )
-                    }
+                    )
+                    return
+                } else{
+                    Log.e("TAG", "onConversionDataSuccess: IS NON ORGANIC, CONDITION FAILED.", )
+                    onError()
+                    return
                 }
 
-                onError()
-                return
             }
 
             override fun onAppOpenAttribution(p0: MutableMap<String, String>?) {
+                Log.e("TAG", "onAppOpenAttribution: onAppOpenAttribution", )
                 res = FlyerStatus.ERROR
                 onError()
                 return
             }
 
             override fun onAttributionFailure(errorMessage: String?) {
+                Log.e("TAG", "onAttributionFailure: onAttributionFailure", )
                 res = FlyerStatus.ERROR
                 onError()
                 return
@@ -77,6 +87,7 @@ object FlyerExtension {
             }
 
             override fun onConversionDataFail(errorMessage: String?) {
+                Log.e("TAG", "onConversionDataFail: onConversionDataFail", )
                 res = FlyerStatus.ERROR
                 onError()
                 return
@@ -88,6 +99,8 @@ object FlyerExtension {
         AppsFlyerLib.getInstance().setDebugLog(true)
     }
 
+
+    @Deprecated("this is appsflyer for DECODE DEV KEY")
     fun setExtensionDecode(
         value: String,
         context: Context,
@@ -97,6 +110,7 @@ object FlyerExtension {
         var res: FlyerStatus? = null
         customTimer {
             if (res == null) {
+                Log.e("TAG", "setExtensionDecode: res == null. Time OUT", )
                 onError()
             }
             return@customTimer
@@ -174,13 +188,22 @@ object CheckPush {
 
     private fun detectDeviceLanguage(context: Context): Boolean {
         val currentLocale: Locale = context.resources.configuration.locales[0]
+        Log.e("TAG", "detectDeviceLanguage: ${currentLocale.language}", )
         return currentLocale.language == "ru"
     }
 
     fun checkPush(context: Context): Boolean {
+        Log.e("TAG", "checkPush: ${detectDeviceLanguage(context)}", )
         return detectDeviceLanguage(context)
     }
 
 }
 
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun isNDaysPassed(startDate: LocalDate, n: Int): Boolean {
+    val currentDate = LocalDate.now()
+    val endDate = startDate.plusDays(n.toLong())
+    return currentDate.isAfter(endDate) || currentDate.isEqual(endDate)
+}
 
